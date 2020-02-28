28fbfc061322d042ebcda06ec6612cd254dd5ed9
gonzaz704 <gonzaz704@gmail.com>
2020-02-28 12:44:40 -0300

    First commit
    problem rendering favs

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => ( //<> esto se llama "fragments" y es para que no haya un "div" mas en el nodo de react dom. Puede ser solo con <> o con React.fragment
+    <>
+        <h1>No encontrado</h1>
+    </>
+
+);
+
+export default NotFound;
